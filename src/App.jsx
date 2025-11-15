d7cce4274698df470bcef5c438ed8800db16ea58
munshen <munshen@users.noreply.github.com>
2025-11-15 17:35:08 +0800

    15NOV

--- src/App.jsx
+++ src/App.jsx
@@ -479,8 +479,12 @@ function LoggedInApp({ account }) {
   const [loadingAssign, setLoadingAssign] = useState(false);
   const [loadingTransfer, setLoadingTransfer] = useState(false);
 
-  const companyRepAddress = "0x9Da7d2CA5C22E3134653920B98a7C9d272706329";
-  const isCompanyRep = account.toLowerCase() === companyRepAddress.toLowerCase();
+  const authorizedRegistrars = [
+  "0x9Da7d2CA5C22E3134653920B98a7C9d272706329", // Owner
+  "0x11f8bfee6159076fa6b659f6920be1341ec5e954"  // New Moderator
+  ];
+
+  const isCompanyRep = authorizedRegistrars.some(addr => addr.toLowerCase() === account.toLowerCase());
 
   async function registerDevice() {
     setInfo(null);
